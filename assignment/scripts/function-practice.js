0c8ce342a8464075929f9c7913071ef220b26205
Shong Lee <Shonglee17@gmail.com>
2022-11-16 12:44:35 -0600

    finish stretch goals except 11, will try to come back

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -99,20 +99,44 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-let numbers = [1,2,3]
-function sumAll() {
+
+function sumAll(...numbers) {
   let sum = 0
-  for (i=0; i<numbers.length ; i++)
+  for ( i=0; i<numbers.length ; i++){
+  sum += numbers[i];
+  }//end for
   // TODO: loop to add items
-  return sum + numbers[i];
-}
-console.log('test;', sumAll(numbers.push[4]) )
+  return sum;
+}//end function
+console.log('total value:', sumAll(1,2,3) )
+
+
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
+function sumThis(...ary){
+  let overZero = ary.filter(checkZero);
+    
+  function checkZero(ary){
+    return ary > 0
+  }//end function checkZero
+ return overZero
+}//end function sumThis
+console.log('put these positive arguments in the array:', sumThis(3,-2,-3,5,10)) ;
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+//https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/javascript link to the question
+//There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+function findThis(...unique){
+    function useThis(unique){
+      return odd; 
+    }
+  return 
+}
+console.log('input these arugments', findThis(1,1,2,2,3,3,4,4,5))
